de52a4792142c5b10df997adc0aaa32734cb1877
David A. Russell <david@aprsworld.com>
2015-09-15 17:31:10 -0500

    No longer clones merged data.

--- merge.js
+++ merge.js
@@ -5,27 +5,23 @@
  *
  * Merges all objects or primatives into dst in order of appearance.  It will
  * intentionally ignore any properties that are explicitly set to undefined.
- * It does a 'deep merge' and will leave as much of the dst object intact and
- * will only copy primatives.  If an object is merged in that does not exist
- * in target, it will create a clone of the object being merged.  Attempts to
- * preserve self-referencing structures (XXX: Needs thought and work).
+ * It does a 'deep merge' and will leave as much of the dst object intact.
+ * Attempts to handle self-referencing structures (XXX: Needs thought and work).
  *
  * Return Value:
  *	Returns undefined if no arguments are passed in.
- *	If the last argument is a primative, it will return that.
+ *	If the last non undefined argument is a primative, it will return that.
  *	If a primative is passed in anywhere else it will return a merged clone.
  *	Otherwise it returns target with all the remaining arguments merged in.
  *
  * Hooks:
  *	before(prop, dst, src): Called for each property after checking to make  *		sure the dst and src properties are not the same and that the
- *		src property is not a reference to the root dst object (in which
- *		case dst is just set with the reference). prop is the name of
- *		the property.  The return value is what is merged which normally
- *		should be src, but can be something else.
- *	after(prop, clone, merge): Called after processing each property
- *		and creating a clone (XXX: This needs thought as ideally the
- *		target should be preserved as much as possible).  The return
- *		value is what the property is actually assigned.
+ *		src property is not a reference to the target (in which
+ *		case in which case the property is just set to src). prop is
+ *		the name of the property.  The return value is what is merged
+ *		which normally should be src, but can be something else.
+ *	after(prop, clone, merge): Called after processing each property.
+ *		The return value is what the property is actually assigned.
  *	XXX: The merging of target and obj0 through objn currently do not
  *		have any hooks.
  *
@@ -49,7 +45,7 @@ function object_merge () {
 	var length = arguments.length;
 	var target = arguments[0];
 	var hooks = object_merge_hooks;
-	var last; // If this is defined, we return it instead of target
+	var last; // If this is defined, return it instead of target
 
 	// Can't merge less than 2 objects
 	if (length < 2) {
@@ -73,7 +69,6 @@ function object_merge () {
 
 		// Ignore extension if it's undefined or the same as the target
 		if (extension === undefined || extension === target) {
-			// XXX: Should we warn if extension is target?
 			continue;
 		}
 
@@ -98,8 +93,8 @@ function object_merge () {
 				continue;
 			}
 
-			// Prevent an infinite loop
-			// XXX: How to properly handle self-referencing structs
+			// Properly handle self-referencing structures
+			// XXX: Figure this out...
 			if (src === target) {
 				target[prop] = target;
 				continue;
@@ -108,26 +103,19 @@ function object_merge () {
 			// Call hook before processing object
 			var merge = hooks.before(prop, dst, src);
 
-			// Do the actual merge
-			// XXX: Properly handle cloning... See comment at top.
-			if (merge && typeof merge === 'object') {
-
-				// Cloning src
-				var clone = {};
-
-				// We actually need an Array object...
-				if (Array.isArray(dst) || Array.isArray(merge)) {
-					clone = [];
-				}
-
-				// Do the actual clone and merge...
-				clone = object_merge(clone, dst, merge);
-
-				// Call hook after processing object
-				merge = hooks.after(prop, clone, merge);
+			// Merge
+			var clone = dst;
+			if (dst && typeof dst === 'object') {
+				clone = object_merge(dst, merge);
+			} else {
+				clone = merge;
 			}
 
-			if (merge !== undefined) {
+			// Call hook after processing object
+			merge = hooks.after(prop, clone, merge);
+
+			// Update the property
+			if (merge !== undefined && (!dst || typeof dst !== 'object')) {
 				target[prop] = merge;
 			}
 		}
